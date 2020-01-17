8b11175456aace90a7db41594f8d91e3faa210cb
Nimish Gupta <ngupta@gammastack.com>
2020-01-17 18:52:42 +0530

    Implemented map function of array

---
+++ my_map.rb
@@ -0,0 +1,20 @@
+class Array
+  def my_map(&block)
+    i = 0
+    output = []
+    while i < size
+      output[i] = block.call at(i)
+      i += 1
+    end
+    output
+  end
+
+  def my_map2
+    output = []
+    self.each { |i| output << (yield i) }
+    output
+  end
+end
+
+p [10, 20, 30].my_map2 { |i| i * 2 }
+p [1, 5, 6, 9].my_map { |i| i * i }
